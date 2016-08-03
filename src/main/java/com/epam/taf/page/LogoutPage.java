344151798ffb08363588dd4b43483765bcf92439
larisabigvava <bigvavalarisa@mail.ru>
2016-08-03 20:50:12 +0300

    bugs fixed

--- src/main/java/com/epam/taf/page/LogoutPage.java
+++ src/main/java/com/epam/taf/page/LogoutPage.java
@@ -1,4 +1,4 @@
-package com.epam.taf.page;
+package page;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
--- src/main/java/com/epam/taf/util/DriverProvider.java
+++ src/main/java/com/epam/taf/util/DriverProvider.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+
 public class DriverProvider {
     public static WebDriver driver = null;
 
